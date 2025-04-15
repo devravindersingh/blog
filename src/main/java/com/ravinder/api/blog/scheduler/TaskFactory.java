c64bc8e6b17ad3e25633112be570fc016c92327f
Ravinder Singh <ravindersngh402@outlook.com>
2025-04-15 22:53:01 +0530

    removed FileWriteTask and TaskFactory: #4

--- src/main/java/com/ravinder/api/blog/scheduler/TaskFactory.java
+++
@@ -1,7 +0,0 @@
-package com.ravinder.api.blog.scheduler;
-
-public class TaskFactory {
-    public static Task createFileTask(String id, String description, int priority, String fileName){
-        return new Task(id, description, priority, new FileWriteTask(fileName));
-    }
-}
